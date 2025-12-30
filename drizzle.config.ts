b8a8efdcf6facc26a87d8a26e88eac2432cbe993
Pumidol <pumidol.l@gmail.com>
2025-12-30 05:35:15 +0000

    init better auth

---
+++ drizzle.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  out: "./drizzle",
+  schema: "./lib/db/schema/index.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL as string,
+    ssl: false,
+  },
+});
